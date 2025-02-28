695ed2512192315e63de7a65fe5522402beecadf
SIShahed16261 <shahedshariful@gmail.com>
2025-03-01 01:55:35 +0600

    7th commit

--- complete_btn.js
+++ complete_btn.js
@@ -1,5 +1,7 @@
-document.getElementById("complete").addEventListener("click", function (event) {
-    event.preventDefault();
+
+
+function updateTask(buttonId) {
+
     const task = document.getElementById("Task").innerText;
     const convertedTask = parseInt(task);
     const newTaskvalue = convertedTask - 1;
@@ -7,10 +9,37 @@ document.getElementById("complete").addEventListener("click", function (event) {
 
     const addition = document.getElementById("adding").innerText;
     const convertedAdd = parseInt(addition);
-    const newaddedValue = convertedAdd+1;
+    const newaddedValue = convertedAdd + 1;
     document.getElementById("adding").innerText = newaddedValue;
 
-    const button = document.getElementById("complete");
+    const button = document.getElementById(buttonId);
     button.disabled = true;
+    alert('Congratulations! You have completed the task.');
+}
 
-})
+
+document.getElementById("complete1").addEventListener("click", function (event) {
+    event.preventDefault();
+    updateTask(this.id);
+});
+document.getElementById("complete2").addEventListener("click", function (event) {
+    event.preventDefault();
+    updateTask(this.id);
+});
+document.getElementById("complete3").addEventListener("click", function (event) {
+    event.preventDefault();
+    updateTask(this.id);
+});
+document.getElementById("complete4").addEventListener("click", function (event) {
+    event.preventDefault();
+    updateTask(this.id);
+});
+document.getElementById("complete5").addEventListener("click", function (event) {
+    event.preventDefault();
+    updateTask(this.id);
+});
+document.getElementById("complete6").addEventListener("click", function (event) {
+    event.preventDefault();
+    updateTask(this.id);
+});
+
