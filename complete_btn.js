9dc35ac56e378580d37f83c03e3ddac2ba7c81aa
SIShahed16261 <shahedshariful@gmail.com>
2025-03-01 10:55:32 +0600

    8th commit

--- complete_btn.js
+++ complete_btn.js
@@ -1,4 +1,10 @@
 
+const now = new Date();
+const options = { weekday: "short", month: "long", day: "numeric", year: "numeric" };
+const fullDate = now.toLocaleDateString("en-US", options);
+document.getElementById("date-time").innerText = ` ${fullDate} `;
+
+
 
 function updateTask(buttonId) {
 
@@ -21,25 +27,53 @@ function updateTask(buttonId) {
 document.getElementById("complete1").addEventListener("click", function (event) {
     event.preventDefault();
     updateTask(this.id);
+    logHistory("title1");
 });
 document.getElementById("complete2").addEventListener("click", function (event) {
     event.preventDefault();
     updateTask(this.id);
+    logHistory("title2");
+
 });
 document.getElementById("complete3").addEventListener("click", function (event) {
     event.preventDefault();
     updateTask(this.id);
+    logHistory("title3");
 });
 document.getElementById("complete4").addEventListener("click", function (event) {
     event.preventDefault();
     updateTask(this.id);
+    logHistory("title4");
 });
 document.getElementById("complete5").addEventListener("click", function (event) {
     event.preventDefault();
     updateTask(this.id);
+    logHistory("title5");
 });
 document.getElementById("complete6").addEventListener("click", function (event) {
     event.preventDefault();
     updateTask(this.id);
+    logHistory("title6");
+
+
 });
 
+function logHistory(id){
+    const title = document.getElementById(id).innerText;
+    const p = document.createElement("p");
+
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, "0"); 
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+    const time = `${hours}:${minutes}:${seconds}`; 
+
+
+    p.innerText = `You have completed the task ${title} at ${time}`;
+    const log = document.getElementById("log");
+    log.appendChild(p);
+}
+
+document.getElementById("clearHistory").addEventListener("click", function () {
+    document.getElementById("log").innerHTML = " "; 
+});
